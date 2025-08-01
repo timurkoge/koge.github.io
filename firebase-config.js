1b2a72119b86d2248a2778e4a44aba22aa6d521b
timurkoge <tim.ishimov@gmail.com>
2025-08-01 19:30:37 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAHY0Q6KSixh_qYDFcrUu3ymAO1NVVtZkE",
-  authDomain: "koge-f8d21.firebaseapp.com",
-  projectId: "koge-f8d21",
-  storageBucket: "koge-f8d21.firebasestorage.app",
-  messagingSenderId: "677551017205",
-  appId: "1:677551017205:web:2474e6d8f134196c3489bc",
-  measurementId: "G-PS9NWMRD1G"
-};
